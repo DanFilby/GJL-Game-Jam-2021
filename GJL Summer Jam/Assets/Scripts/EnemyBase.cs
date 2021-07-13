a2a43b9662be2453a9c3efe6b890c4eeff9e6e18
Daniel Filby <DF245193@falmouth.ac.uk>
2021-07-13 22:33:06 +0100

    started work on enemies

---
+++ GJL Summer Jam/Assets/Scripts/EnemyBase.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    public Color cornerColour;
+    public GameObject cornerParent;
+
+
+    void Start()
+    {
+        ChanageCornerColours(cornerColour);
+
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+
+    /// <summary>
+    /// Change colours of this enemy's corners
+    /// </summary>
+    public void ChanageCornerColours(Color newColour)
+    {
+        //gets each corner and changes its colour
+        foreach(Transform corner in cornerParent.GetComponentInChildren<Transform>())
+        {
+            corner.gameObject.GetComponent<Renderer>().material.color = newColour;
+        }
+    }
+
+
+}
