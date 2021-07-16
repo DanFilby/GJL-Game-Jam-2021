e0631cb2782db657391c17a369bbe0ba95d5f671
Daniel Filby <DF245193@falmouth.ac.uk>
2021-07-16 16:12:11 +0100

    created level 1. added dash mechanic. most functionality is implemented now

--- GJL Summer Jam/Assets/Scripts/PlayerMovement.cs
+++ GJL Summer Jam/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,10 @@ public class PlayerMovement : MonoBehaviour
     public KeyCode sprintKey = KeyCode.LeftShift;
     public float moveSpeed;
     public float runSpeedMultiplier;
+    public float dashForceMultiplier;
+    public float dashRate;
+    private float prevDashTime;
+
     public float mouseSensitivity;
     private float xRotation = 0f;
 
@@ -28,6 +32,7 @@ public class PlayerMovement : MonoBehaviour
     {
         MoveCheck();
         CameraCheck();
+        DashCheck();
     }
 
     /// <summary>
@@ -61,12 +66,30 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(movePos);
     }
 
-
     /// <summary>
-    /// toggles whether the cursor can move across screen and be seen.
+    /// Checks for dash input
     /// </summary>
-    /// <param name="state"> true to allow it to move and be shown </param>
-    public static void ToggleCursor(bool state)
+    private void DashCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time - prevDashTime >= dashRate)
+        {
+            //store how much forward and sideways is inputted into a vector. 
+            Vector3 dashDirection = transform.right * Input.GetAxisRaw("Horizontal") + transform.forward * Input.GetAxisRaw("Vertical");
+
+            //create the force and apply to rigidbody
+            dashDirection *= dashForceMultiplier;
+            rb.AddForce(dashDirection,ForceMode.Impulse);
+
+            //store time of dash to limit to set rate
+            prevDashTime = Time.time;
+        }
+    }
+
+        /// <summary>
+        /// toggles whether the cursor can move across screen and be seen.
+        /// </summary>
+        /// <param name="state"> true to allow it to move and be shown </param>
+        public static void ToggleCursor(bool state)
     {
         Cursor.lockState = state ? CursorLockMode.None : CursorLockMode.Locked;
         Cursor.visible = state;
